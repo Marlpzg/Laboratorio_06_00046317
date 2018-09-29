8ed4281e2527d44c7c13536abfa178709187b89a
Mario <00046317@uca.edu.sv>
2018-09-28 22:30:19 -0600

    Se ha completado el Ejercicio 12.

--- Ejercicio_12.js
+++ Ejercicio_12.js
@@ -9,7 +9,20 @@ function agregarUsuario(){
     ape = prompt("Ingrese el apellido.");
     fecha = prompt("Ingrese la fecha de nacimiento.");
     tel = prompt("Ingrese el número de teléfono.");
-    correo = prompt("Ingrese el correo electrónico.");
+    while(true){
+        correo = prompt("Ingrese el correo electrónico.").toLowerCase();
+        for(let x of usuarios){
+            if(x.Correo == correo){
+                correo = null;
+                break;
+            }
+        }
+        if(correo != null){
+            break;
+        }else{
+            alert("Ese correo ya ha sido registrado.")
+        }
+    }
     
     usuarios.push({
         Nombre: nom,
@@ -23,6 +36,96 @@ function agregarUsuario(){
     console.log(usuarios);
 }
 
+function modificarUsuario(){
+    let codigo, usuario;
+    alert("Bienvenido al formulario de edición de usuario, por favor llene los datos que se le soliciten.")
+    codigo = prompt("Ingrese el código del usuario que modificará.");
+    if(codigo >= 0 && codigo < usuarios.length){
+        usuario = usuarios[codigo];
+        usuario.Nombre = prompt("Ingrese el nombre.",usuario.Nombre);
+        usuario.Apellido = prompt("Ingrese el apellido.", usuario.Apellido);
+        usuario.Fecha_Nacimiento = prompt("Ingrese la fecha de nacimiento.", usuario.Fecha_Nacimiento);
+        usuario.Telefono = prompt("Ingrese el número de teléfono.", usuario.Telefono);
+        while(true){
+            correo = prompt("Ingrese el correo electrónico.",usuario.Correo).toLowerCase();
+            if(correo != usuario.Correo){
+                for(let x of usuarios){
+                    if(x.Correo == correo){
+                        correo = null;
+                        break;
+                    }
+                }
+                if(correo != null){
+                    break;
+                }else{
+                    alert("Ese correo ya ha sido registrado.")
+                }
+            }else{
+                break;
+            }
+        }
+        usuario.Correo = correo;
+
+        alert("Se ha terminado de editar a la persona.");
+        console.log(usuarios);
+    }else{
+        alert("No se ha ingresado un código válido.");
+    }
+}
+
+function eliminarUsuario(){
+    let codigo, usuario, conf;
+    alert("Bienvenido al formulario de borrado de usuario, por favor llene los datos que se le soliciten.")
+    codigo = prompt("Ingrese el código del usuario que eliminará.");
+    if(codigo >= 0 && codigo < usuarios.length){
+        usuario = usuarios[codigo];
+        
+        alert("A continuación se le mostrará el usuario que ha seleccionado.")
+
+        alert(buscarUsuario(usuario.Correo));
+
+        conf = confirm("¿Desea eliminarlo?");
+
+        if(conf){
+            alert("Se ha eliminado a la persona.");
+            usuarios.pop(codigo);
+        }else{
+            alert("No se realizaron cambios.");
+        }
+        console.log(usuarios);
+    }
+
+}
+
+function buscarUsuario(correo){
+    
+    for(let x of usuarios){
+        if(x.Correo == correo){
+            let msg = "";
+            msg = msg + "Código: " + usuarios.indexOf(x) + "\n";
+            msg = msg + "Nombre: " + x.Nombre + " " + x.Apellido + "\n";
+            msg = msg + "Fecha de Nacimiento: " + x.Fecha_Nacimiento + "\n";
+            msg = msg + "Teléfono: " + x.Telefono + "\n";
+            msg = msg + "Correo: " + x.Correo + "\n";
+            return msg;
+        }
+    }
+    return "El usuario especificado no se ha encontrado.";
+}
+
+function verUsuarios(){
+    alert("A continuación se le mostrarán todas las fichas de las personas registradas.")
+    for(let x of usuarios){
+        let msg = "";
+        msg = msg + "Código: " + usuarios.indexOf(x) + "\n";
+        msg = msg + "Nombre: " + x.Nombre + " " + x.Apellido + "\n";
+        msg = msg + "Fecha de Nacimiento: " + x.Fecha_Nacimiento + "\n";
+        msg = msg + "Teléfono: " + x.Telefono + "\n";
+        msg = msg + "Correo: " + x.Correo + "\n";
+        alert(msg);
+    }
+}
+
 function iniciarPrograma(){
     var usuarios = [];
     while(true){
@@ -33,12 +136,18 @@ function iniciarPrograma(){
                     agregarUsuario();
                     break;
                 case "2":
+                    alert("Por favor llene los datos que se le soliciten para buscar su resultado.")
+                    let correoB = prompt("Ingrese el correo del usuario.").toLowerCase();
+                    alert(buscarUsuario(correoB));
                     break;
                 case "3":
+                    modificarUsuario();
                     break;
                 case "4":
+                    eliminarUsuario();
                     break;
                 case "5":
+                    verUsuarios();
                     break;
                 default:
                     alert("Por favor ingrese un dato válido.")
